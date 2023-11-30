b62aa2785ba817e23c3ec39728f868e44829ead9
Eugene Polevikov <eugene.polevikov@yandex.ru>
2023-11-30 12:56:55 +0300

    Remove everything to start from scratch

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import './index.css';
-import App from './App.js';
+import ProteinFoldingStage from './ProteinFoldingStage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ProteinFoldingStage/>
   </React.StrictMode>
 );
