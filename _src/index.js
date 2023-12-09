2918c7adb6c1d3c010ba0b5a3635d3983116593b
Eugene Polevikov <eugene.polevikov@yandex.ru>
2023-12-09 19:33:53 +0300

    Setup initial environment

--- _src/index.js
+++ _src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-function App() {
-  return (
-    <h1>Hello!</h1>
-  );
-}
+import './index.css';
+import ProteinFoldingStage from './ProteinFoldingStage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App/>
+    <ProteinFoldingStage/>
   </React.StrictMode>
 );
