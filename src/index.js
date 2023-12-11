75c5703016b208eaf51d67f548f25712b62de029
Eugene Polevikov <eugene.polevikov@yandex.ru>
2023-12-11 17:30:00 +0300

    Make minor import fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import FoldingStage from './FoldingStage';
+import FoldingStage from './components/FoldingStage';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
